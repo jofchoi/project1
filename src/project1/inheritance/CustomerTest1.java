6a14c2b7f715c3a733a26209b0794deff28b1153
jofchoi <jofchoi@gmail.com>
2020-08-16 18:36:05 +0900

    array and inheritage
    examples update

---
+++ src/project1/inheritance/CustomerTest1.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+public class CustomerTest1 {
+	public static void main(String[] args) {
+		Customer customerLee = new Customer();
+		customerLee.setCustomerID(10010);
+		customerLee.setCustomerName("이순신");
+		customerLee.bonusPoint = 1000;
+		System.out.println(customerLee.showCustomerInfo());
+		
+		VIPCustomer customerKim = new VIPCustomer();
+		customerKim.setCustomerName("김유신");
+		customerKim.setCustomerID(10020);
+		customerKim.bonusPoint = 1000;
+		System.out.println(customerKim.showCustomerInfo());
+	}
+}
