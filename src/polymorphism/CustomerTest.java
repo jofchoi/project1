bdc23b75707f34f75c5525b4abceae526c9c3e9f
jofchoi <jofchoi@gmail.com>
2020-08-19 22:28:36 +0900

    poly + array
    exmaple update

--- src/polymorphism/CustomerTest.java
+++ src/polymorphism/CustomerTest.java
@@ -1,25 +1,34 @@
 package polymorphism;
+import java.util.ArrayList;
 
 public class CustomerTest {
 	public static void main(String[] args) {
-		Customer customerLee = new Customer(10010, "LEE");
-		customerLee.bonusPoint = 1000;
+		ArrayList<Customer> customerList = new ArrayList<Customer>();
+
+		Customer customerLee = new Customer(10010, "Lee");
+		Customer customerShin = new Customer(10020, "Shin");
+		Customer customerHong = new GoldCustomer(10030, "Hong");
+		Customer customerYoul = new GoldCustomer(10040, "Youl");
+		Customer customerKim = new VipCustomer(10050, "Kim", 12345);
 		
-		System.out.println(customerLee.showCustomerInfo());
+		customerList.add(customerLee);
+		customerList.add(customerShin);
+		customerList.add(customerHong);
+		customerList.add(customerYoul);
+		customerList.add(customerKim);
 		
-		Customer customerKim = new VipCustomer(10020, "KIM", 12345);
-		customerKim.bonusPoint = 1000;
+		System.out.println("===========info=========");
+		for(Customer customer : customerList) {
+			System.out.println(customer.showCustomerInfo());
+		}
 		
-		System.out.println(customerKim.showCustomerInfo());
-		System.out.println("========할인율과 보너스 포인트 계산");
-		
+		System.out.println("=====discountrate and bonusPoint=====");
 		int price = 10000;
-		int leeprice = customerLee.calcPrice(price);
-		int kimprice = customerKim.calcPrice(price);
+		for(Customer customer : customerList) {
+			int cost = customer.calcPrice(price);
+			System.out.println(customer.customerName + "님이" + cost + "냇음");
+			System.out.println(customer.customerName + "님의 현재 보너스포인트는" + customer.bonusPoint);
+		}
 		
-		System.out.println((customerLee.customerName) +"님의  " + leeprice );
-		System.out.println(customerLee.showCustomerInfo());
-		System.out.println((customerKim.customerName) + "님의 " + kimprice);
-		System.out.println(customerKim.showCustomerInfo());
 	}
 }
