2a4ebf11a6461db03a9cc9f7d78fb6dac3b3a17c
jofchoi <jofchoi@gmail.com>
2020-08-22 11:13:13 +0900

    SD moregetda2
    imporving...

--- src/sdExample/AnonymousBoard.java
+++ src/sdExample/AnonymousBoard.java
@@ -1,5 +1,19 @@
+/**
+ * 
+ * - 익명게시판(AnonymousBoard) 클래스 생성, 게시판 상속
+
+ */
+
 package sdExample;
 
 public class AnonymousBoard {
+
+	public void char[] showInfo() {
+		return null;
+	}
+
+	public void char[] showInfo(int i) {
+		return null;
+	}
 
 }
--- src/sdExample/FreeBoard.java
+++ src/sdExample/FreeBoard.java
@@ -1,5 +1,18 @@
+/**- 자유게시판(FreeBoard) 클래스 생성, 게시판 상속
+
+*/
 package sdExample;
 
 public class FreeBoard {
+
+	public static String[] showInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static char[] showInfo(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
