f3f76baa6ff460cddd2cb0224a99d8824cf0a575
jofchoi <jofchoi@gmail.com>
2020-09-01 23:43:31 +0900

    Template Method exmaple
    example upload

--- src/template/AICar.java
+++ src/template/AICar.java
@@ -13,4 +13,9 @@ public class AICar extends Car{
 		System.out.println("스스로 멈춥니다.");		
 	}
 
+	@Override
+	public void wiper() {
+		System.out.println("비나 눈의 양에 따라 빠르기가 자동으로 조절됩니다.");
+	}
+
 }
--- src/template/ManualCar.java
+++ src/template/ManualCar.java
@@ -13,4 +13,9 @@ public class ManualCar extends Car{
 		System.out.println("브레이크로 정지합니다.");
 	}
 
+	@Override
+	public void wiper() {
+		System.out.println("사람이 빠르기를 조절합니다.");
+	}
+
 }
