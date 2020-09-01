73e41f40421a7947e7c49f30a591cfaaec055c29
jofchoi <jofchoi@gmail.com>
2020-09-01 22:39:06 +0900

    template example update
    examaple update

--- src/abstractex/DeskTop.java
+++ src/abstractex/DeskTop.java
@@ -1,5 +1,15 @@
 package abstractex;
 
-public class DeskTop {
+public class DeskTop extends Computer{
 
+	@Override
+	public void display() {
+		System.out.println("DeskTop display()");
+	}
+
+	@Override
+	public void typing() {
+		System.out.println("DeskTop typing()");
+	}
+	
 }
